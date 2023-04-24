b7b7e090cf6f0155d3c27a0b1eec95c3d7e4081d
Lucho Aquino <luis.gonzales.a@uni.pe>
2023-04-23 21:37:49 -0500

    Se agrega el proyecto

---
+++ Clase1.py
@@ -0,0 +1,23 @@
+print("Hola","soy",1,sep="")
+print("Hola"+"soy"+"1")
+print("Dan\viela")
+print("Hola","mi","nombre",sep='--',end='***')
+print("es","Nombre", sep="--",end="#")
+print("\\") #El \ es el caracter de excape  #Imprime el \
+print(r"Hola \nque tal")
+print('\a')
+print('hola\bhola') #b borra el caracter antecesor
+print('hola\fhola') #Salto de página
+#Listas
+numb = [1,2,3,4,5]
+varios= [1,True,2.25,'Hola',[1,-2]]
+print(numb[-1]) #De derecha a izquierda comienza con -1, -2 , -3 y asi
+print(numb[0]) # De izquierda a derecha comienza desde 0
+print(varios[-1][1]) #Del ultimo miembro el termino 1 seria -2
+
+
+print("a\u1F600")
+print('''aaa
+vvvv
+vvvv
+vvv''')
